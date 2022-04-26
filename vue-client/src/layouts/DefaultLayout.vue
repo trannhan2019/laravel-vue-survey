1a78a740cccdd91b94c13562207987daa9482413
TranNhan <trannhan.s3@gmail.com>
2022-04-26 21:06:00 +0700

    26.04.2022

--- vue-client/src/layouts/DefaultLayout.vue
+++ vue-client/src/layouts/DefaultLayout.vue
@@ -1,13 +1,6 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <!--
-    This example requires updating your template:
 
-    ```
-    <html class="h-full bg-gray-100">
-    <body class="h-full">
-    ```
-  -->
   <div class="min-h-full">
     <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -60,7 +53,19 @@
 
       <DisclosurePanel class="md:hidden">
         <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+          <router-link
+            v-for="item in navigation"
+            :key="item.name"
+            :to="item.to"
+            active-class="bg-gray-900 text-white"
+            :class="[
+                    this.$route.name === item.to.name
+                      ? ''
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'px-3 py-2 rounded-md text-sm font-medium',
+                  ]"
+          >{{ item.name }}
+          </router-link>
         </div>
         <div class="pt-4 pb-3 border-t border-gray-700">
           <div class="flex items-center px-5">
@@ -91,19 +96,12 @@
 <script>
   import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
   import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+  import {useStore} from "vuex";
+  import {computed} from 'vue'
 
-  const user = {
-    name: 'Tom Cook',
-    email: 'tom@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  }
   const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-    { name: 'Reports', href: '#', current: false },
+    { name: 'Dashboard', to:{name:"Dashboard"}},
+    { name: 'Surveys', to: {name:"Surveys"}},
   ]
   const userNavigation = [
     { name: 'Your Profile', href: '#' },
@@ -125,8 +123,9 @@
       XIcon,
     },
     setup() {
+      const store = useStore();
       return {
-        user,
+        user:computed(()=>store.state.user.data),
         navigation,
         userNavigation,
       }
