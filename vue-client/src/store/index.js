3064e6f1d7ffedef7b433f90864ea625f77a0601
TranNhan <trannhan.s3@gmail.com>
2022-04-28 21:27:04 +0700

    dang hoan thien Register, chua lien ket store va Alert, object key

--- vue-client/src/store/index.js
+++ vue-client/src/store/index.js
@@ -1,22 +1,21 @@
-import {createStore} from "vuex";
+import { createStore } from "vuex";
 
 const store = createStore({
-  state:{
-    user:{
-      data:{
-        name: 'Tom Cook',
-        email: 'tom@example.com',
+  state: {
+    user: {
+      data: {
+        name: "Tom Cook",
+        email: "tom@example.com",
         imageUrl:
-          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
       },
-      token: 123
-    }
+      token: null,
+    },
   },
-  getters:{},
-  mutations:{},
-  actions:{},
-  modules:{}
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
 
 export default store;
-
